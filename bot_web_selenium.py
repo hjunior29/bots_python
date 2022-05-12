f310e69c52535217335096e1fb403a01067842cf
junior <99821795+hjunior29@users.noreply.github.com>
2022-05-12 15:56:36 -0300

    atualizacao de extracao de dados, agora extrai dados e importa para um arquivo .csv

--- bot_web_selenium.py
+++ bot_web_selenium.py
@@ -1,4 +1,6 @@
 #importando as bibliotecas
+import pandas as pd                                 #biblioteca de importação do arquivo .csv
+from bs4 import BeautifulSoup                       #biblioteca de varredura de dados
 from selenium import webdriver                      #biblioteca para controlar o navegador
 from selenium.webdriver.common.by import By         #algumas funcionalidades da biblioteca
 from selenium.webdriver.common.keys import Keys     #algumas funcionalidades da biblioteca
@@ -8,14 +10,17 @@ import time                                         #biblioteca de tempo
 #oferece a escolha de um atalho por categorias
 #pega informações do usuário sobre o produto buscado
 #e sobre a ordem que os produtos vão aparecer
+#o usuário escolhe o nome do arquivo .csv
 def pegar_informacoes():
     global categoria
     global pesquisa
     global ordem
+    global nome_arquivo
     categoria = int(input("Escolha uma categoria:\nIPhone(1)\nCelular(2)\nGeladeira(3)\nNotebook(4)\nTV(5)\nOu digite (0) para pesquisar: "))
     if categoria == 0:
         pesquisa = input("Pesquisar:\n")
     ordem = int(input("\n\nOrdenar o produto por:\nMais relevante(1)\nMenor preço(2)\nMaior preço(3)\n"))
+    nome_arquivo = input("\nQual vai ser o nome do arquivo .csv: ")
 
 #declara a variável driver global
 #atribui o webdriver a variável driver
@@ -65,13 +70,42 @@ def filtrar_produto():
         seta = driver.find_element(By.ID, "orderBy").send_keys(Keys.DOWN)
         time.sleep(1)
     enter = driver.find_element(By.ID, "orderBy").send_keys(Keys.ENTER)
-    print("\033[0;32mPRODUTO FILTRADO.")
+
+#parsea o HTML por meio do BeautifulSoup
+def parsear():
+    global soup
+    page_content = driver.page_source
+    soup = BeautifulSoup(page_content, "html.parser")
+
+#coleta os dados da página
+def coletar_dados():
+    global list_products
+    products = soup.findAll(class_="Cell_Infos__KDy41")
+    list_products = []
+    for product in products:
+        product_name = product.find(class_="Text_Text__VJDNU Text_LabelSmRegular__qvxsr")
+        product_price = product.find(class_="Text_Text__VJDNU Text_LabelMdBold__uMr7_ CellPrice_MainValue__JXsj_")
+        list_products.append([product_name.text, product_price.text])
+
+#cria um DataFrame e aloca os dados em um arquivo .csv
+def criar_csv():
+    products_data = pd.DataFrame(list_products, columns=["Nome do Produto",  "Preço"])
+    products_data.to_csv(f"{nome_arquivo}.csv", index=False)
+    print("\033[0;32m"+products_data)
+
+    print("\n\033[0;32mPROCESSO FINALIZADO")
+
+
 
 #chamando cada função na ordem
 pegar_informacoes()
 abrir_site()
 pesquisar_produto()
 filtrar_produto()
+parsear()
+coletar_dados()
+criar_csv()
+
 
 #outra opção da função de escolher a categoria
 def escolher_categoria2():
@@ -97,3 +131,8 @@ def filtrar_produto2():
     elif ordem == 3:
         ordenar_maior = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[1]/div/div/div/div[2]/select/option[3]").click()
     print("\033[0;32mPRODUTO FILTRADO.")
+
+#tentativa de função para passar a página
+def passar_pagina(): #dando erro!!!
+    for i in range(4):
+        passar = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[2]/div[4]/div/ul/li[6]/a").click()
